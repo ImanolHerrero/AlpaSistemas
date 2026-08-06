0b4f81c6e52d3688d501042bbd9e7d68b3a15fb7
Imanol <imano.arias@gmail.com>
2026-08-06 06:50:33 -0300

    Build and fix type error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Hero from "./components/Hero.tsx";
-import WhyChooseUs from "./components/whyChooseUs.tsx";
+import WhyChooseUs from "./components/WhyChooseUs.tsx";
 import Services from "./components/Services.tsx";
 import WorkProcess from "./components/WorkProcess.tsx";
 import Partner from "./components/Partner.tsx";
